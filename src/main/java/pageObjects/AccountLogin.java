b1b9ff3618ddb7d5b65412bf6e92de537d05ec2e
Kostantin K <110024628+kostantink89@users.noreply.github.com>
2022-12-29 20:06:53 +0200

    initial changes to account login

--- src/main/java/pageObjects/AccountLogin.java
+++ src/main/java/pageObjects/AccountLogin.java
@@ -2,6 +2,7 @@ package pageObjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class AccountLogin {
 
@@ -12,16 +13,39 @@ public class AccountLogin {
         this.driver = driver;
     }
 
-    By accountLoginText = By.xpath("//span[@class='maintext']");
-    By newCustomerText = By.xpath("//h2[text()='I am a new customer.']");
+    private By accountLoginText = By.xpath("//span[@class='maintext']");
+    private By newCustomerText = By.xpath("//h2[text()='I am a new customer.']");
 
-    By continueButton = By.xpath("//button[@type='submit'][@title='Continue']");
+    private By continueButton = By.xpath("//button[@type='submit'][@title='Continue']");
 
-    By loginName = By.id("loginFrm_loginname");
-    By password = By.id("loginFrm_password");
+    private By loginName = By.id("loginFrm_loginname");
+    private By password = By.id("loginFrm_password");
 
-    By loginButton = By.xpath("//button[@type='submit'][@title='Login']");
+    private By loginButton = By.xpath("//button[@type='submit'][@title='Login']");
 
+    public WebElement getAccountLoginText() {
+        return driver.findElement(accountLoginText);
 
+    }
+
+    public WebElement newCustomerText() {
+        return driver.findElement(newCustomerText);
+    }
+
+    public WebElement getContinueButton() {
+        return driver.findElement(continueButton);
+    }
+
+    public void setLoginName(String text) {
+        driver.findElement(loginName).sendKeys(text);
+    }
+
+    public void setPassword(String text) {
+        driver.findElement(password).sendKeys(text);
+    }
+
+    public WebElement getLoginButton() {
+        return driver.findElement(loginButton);
+    }
 
 }
