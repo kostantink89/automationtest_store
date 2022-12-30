7b55a38bd5ddee9a85da206811e8e33295d26fee
Kostantin K <110024628+kostantink89@users.noreply.github.com>
2022-12-30 09:25:24 +0200

    adding new changes

--- src/test/java/LoginUser.java
+++ src/test/java/LoginUser.java
@@ -1,21 +1,32 @@
 import base.BasePage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pageObjects.AccountLogin;
 import pageObjects.HomePage;
+import pageObjects.MyAccountPage;
 
 public class LoginUser extends BasePage {
 
 
     @Test
-    public void loginUserTest() {
+    public void loginUserTest() throws InterruptedException {
 
+        Thread.sleep(1500);
         HomePage home = new HomePage(driver);
         home.getLoginOrRegister().click();
+        Thread.sleep(1500);
 
         AccountLogin account = new AccountLogin(driver);
+        Thread.sleep(1500);
         account.enterLoginName("bolton11");
+        Thread.sleep(1500);
         account.enterPassword("boltibolti11");
 
+        Thread.sleep(1500);
+        MyAccountPage myAccount = new MyAccountPage(driver);
+        Thread.sleep(1500);
+        Assert.assertEquals(myAccount.getMyAccountText(), "MY ACCOUNT");
+        Thread.sleep(1500);
 
 
     }
